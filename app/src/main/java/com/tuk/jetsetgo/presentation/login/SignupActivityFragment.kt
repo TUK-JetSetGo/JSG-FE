658a9b7036c89fce358e9243c20b944ca88091fa
ekgml001 <dahea5069@gmail.com>
2025-03-15 14:16:33 +0900

    optimize imports

--- app/src/main/java/com/tuk/jetsetgo/presentation/login/SignupActivityFragment.kt
+++ app/src/main/java/com/tuk/jetsetgo/presentation/login/SignupActivityFragment.kt
@@ -2,7 +2,6 @@ package com.tuk.jetsetgo.presentation.login
 
 import com.tuk.jetsetgo.R
 import com.tuk.jetsetgo.databinding.FragmentSignupActivityBinding
-import com.tuk.jetsetgo.databinding.FragmentSignupThemeBinding
 import com.tuk.jetsetgo.presentation.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
