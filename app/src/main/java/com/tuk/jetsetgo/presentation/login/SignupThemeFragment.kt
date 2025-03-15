6f8037a40463c9e68b5880669cd228c444f56fb1
ekgml001 <dahea5069@gmail.com>
2025-03-15 13:19:32 +0900

    [UI/#12] signupTheme xml 작성

---
+++ app/src/main/java/com/tuk/jetsetgo/presentation/login/SignupThemeFragment.kt
@@ -0,0 +1,18 @@
+package com.tuk.jetsetgo.presentation.login
+
+import com.tuk.jetsetgo.R
+import com.tuk.jetsetgo.databinding.FragmentSignupThemeBinding
+import com.tuk.jetsetgo.presentation.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SignupThemeFragment : BaseActivity<FragmentSignupThemeBinding>(R.layout.fragment_signup_theme) {
+
+    override fun initView() {
+    }
+
+    override fun initObserver() {
+
+    }
+
+}
