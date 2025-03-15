76e7b8b67f9f9948c6e33c3a419d54a1659d6397
ekgml001 <dahea5069@gmail.com>
2025-03-15 14:15:00 +0900

    [UI/#12] signupActivity xml 작성

---
+++ app/src/main/java/com/tuk/jetsetgo/presentation/login/SignupActivityFragment.kt
@@ -0,0 +1,19 @@
+package com.tuk.jetsetgo.presentation.login
+
+import com.tuk.jetsetgo.R
+import com.tuk.jetsetgo.databinding.FragmentSignupActivityBinding
+import com.tuk.jetsetgo.databinding.FragmentSignupThemeBinding
+import com.tuk.jetsetgo.presentation.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SignupActivityFragment : BaseActivity<FragmentSignupActivityBinding>(R.layout.fragment_signup_activity) {
+
+    override fun initView() {
+    }
+
+    override fun initObserver() {
+
+    }
+
+}
