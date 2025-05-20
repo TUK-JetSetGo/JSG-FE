a3f21b9117fee77d2ba379dd3da174b3646c5a55
다희 <163154446+ekgml001@users.noreply.github.com>
2025-05-20 15:39:40 +0900

    [UI/#37] 비상대처 xml 구현 (#40)
    * [FIX/#38] fontSize 수정
    
    * [UI/#37] modifyReason, modifySchedule xml
    
    * [Feat/#37] 선택 모드 구현
    
    * [DEL] 사용 안 하는 코드 제거
    
    * [UI/#37] topbar 추가

---
+++ app/src/main/java/com/tuk/jetsetgo/presentation/myTravel/ModifyReasonFragment.kt
@@ -0,0 +1,42 @@
+package com.tuk.jetsetgo.presentation.myTravel
+
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
+import com.tuk.jetsetgo.R
+import com.tuk.jetsetgo.databinding.FragmentModifyReasonBinding
+import com.tuk.jetsetgo.presentation.base.BaseFragment
+
+class ModifyReasonFragment : BaseFragment<FragmentModifyReasonBinding>(R.layout.fragment_modify_reason) {
+
+    override fun initObserver() {
+
+    }
+
+    override fun initView() {
+        setClickListener()
+        setBackPressedCallback()
+        setRadioGroupListener()
+    }
+
+    private fun setClickListener() {
+        binding.viewModifyReasonNextBtn.setOnClickListener { findNavController().navigate(R.id.reasonToModifySchedule) }
+    }
+
+    private fun setBackPressedCallback() {
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack() // 이전 화면으로 이동
+            }
+        })
+    }
+
+    private fun setRadioGroupListener() {
+        binding.rgModifyReason.setOnCheckedChangeListener { _, checkedId ->
+            if (checkedId != -1) {
+                binding.viewModifyReasonNextBtn.visibility = View.VISIBLE
+            }
+        }
+    }
+
+}
