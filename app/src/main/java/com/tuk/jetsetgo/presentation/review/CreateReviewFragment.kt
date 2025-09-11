46cc85c37d6308d9102a9cb58a53b6a5bf7e22ec
ekgml001 <163154446+ekgml001@users.noreply.github.com>
2025-09-11 20:47:18 +0900

    [UI/#61] createReview

---
+++ app/src/main/java/com/tuk/jetsetgo/presentation/review/CreateReviewFragment.kt
@@ -0,0 +1,18 @@
+package com.tuk.jetsetgo.presentation.review
+
+import com.tuk.jetsetgo.R
+import com.tuk.jetsetgo.databinding.FragmentCreateReviewBinding
+import com.tuk.jetsetgo.presentation.base.BaseFragment
+
+class CreateReviewFragment: BaseFragment<FragmentCreateReviewBinding>(R.layout.fragment_create_review) {
+
+
+    override fun initObserver() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+}
